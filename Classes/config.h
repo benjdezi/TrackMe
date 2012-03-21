e29dbf103200762b765b2c4a08d7a86ac29091a0
Benjamin Dezile <benjdezi@aol.com>
2012-03-21 03:35:53 -0700

    Version number update

--- Classes/config.h
+++ Classes/config.h
@@ -7,7 +7,7 @@
  *
  */
 
-#define VERSION				@"0.1.3"		// Version
+#define VERSION				@"0.1.4"		// Version
 
 #define MILE_TO_YARD		1760			// Conversion from miles to yards
 #define YARD_TO_METER		0.9144			// Conversion from yards to meters
